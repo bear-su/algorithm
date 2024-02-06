e25e1e6a30c01db6e53d35515c8aa850e114d33e
bearsu <bearsu98@gmail.com>
2024-02-06 23:37:55 +0900

    2024-02-06 알고리즘 공부

--- src/review/B1074.java
+++ src/review/B1074.java
@@ -4,8 +4,38 @@ import java.util.Scanner;
 
 public class B1074 {
 
-    public static void main(String[] args) {
+    static int result = 0;
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int R = sc.nextInt();
+        int C = sc.nextInt();
+
+        DFS(N, R, C);
+        System.out.println(result);
     }
 
+
+    private static void DFS(int N, int R, int C) {
+        int boardSize = 1 << N;
+        int half = boardSize / 2;
+
+        if (N == 0) return;
+
+        // 1사 분면
+        if (R < half && C < half) {
+            DFS (N - 1, R, C);
+        } else if (R < half && C >= half) {
+            result += half * half;
+            DFS (N - 1, R, C - half);
+        } else if (R >= half && C < half) {
+            result += half * half * 2;
+            DFS (N - 1, R - half, C);
+        } else {
+            result += half * half * 3;
+            DFS(N - 1, R - half, C - half);
+        }
+    }
 }
--- src/review/P68936.java
+++ src/review/P68936.java
@@ -1,4 +1,31 @@
 package review;
 
-public class P68936 {
+class Solution {
+    int[] answer = new int[2];
+    public int[] solution(int[][] arr) {
+        quad(arr, 0, 0, arr.length);
+        return answer;
+    }
+
+    private boolean zip(int[][] arr, int r, int c, int size) {
+        for (int i = r; i < r + size; i++) {
+            for (int j = c; j < c + size; j++) {
+                if (arr[r][c] != arr[i][j]) return false;
+            }
+        }
+        return true;
+    }
+
+    private void quad(int[][] arr, int r, int c, int size) {
+        if (zip(arr, r, c, size)) {
+            if (arr[r][c] == 1) answer[1]++;
+            else answer[0]++;
+            return;
+        }
+        quad(arr, r, c, size / 2);
+        quad(arr, r, c + size / 2, size / 2);
+        quad(arr, r + size / 2, c, size / 2);
+        quad(arr, r + size / 2, c + size / 2, size / 2);
+    }
+
 }
